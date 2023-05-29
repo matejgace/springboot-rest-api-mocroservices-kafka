543db883bf03e330f2be16bc59f4d236ef2de7b0
mgace <matejgace@gmail.com>
2023-05-29 13:02:29 +0200

    REST API Documentation -Swagger

--- src/main/java/mg/java/springbootrestapi/controller/UserController.java
+++ src/main/java/mg/java/springbootrestapi/controller/UserController.java
@@ -1,6 +1,9 @@
 package mg.java.springbootrestapi.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import mg.java.springbootrestapi.dto.UserDto;
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Get All Users, Delete User"
+)
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -18,12 +26,30 @@ public class UserController {
 
     private UserService userService;
 
+    @Operation(
+            summary = "Create User REST API",
+            description = "Create User REST API is used to save user in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
+
     // build create User REST API
     @PostMapping
     public ResponseEntity<UserDto> createUser( @RequestBody @Valid UserDto user){
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
+
+    @Operation(
+            summary = "Get User By ID REST API",
+            description = "Get User By ID REST API is used to get a single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
     // build get user by id REST API
     // http://localhost:8080/api/users/1
@@ -33,6 +59,14 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get All Users REST API",
+            description = "Get All Users REST API is used to get a all the users from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     // Build Get All Users REST API
     // http://localhost:8080/api/users
     @GetMapping
@@ -40,6 +74,15 @@ public class UserController {
         List<UserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Update User REST API",
+            description = "Update User REST API is used to update a particular user in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
     // Build Update User REST API
     @PutMapping("{id}")
@@ -51,11 +94,34 @@ public class UserController {
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete User REST API",
+            description = "Delete User REST API is used to delete a particular user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     // Build Delete User REST API
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
         userService.deleteUser(userId);
         return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
     }
+
+
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+//                                                                        WebRequest webRequest){
+//
+//        ErrorDetails errorDetails = new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(false),
+//                "USER_NOT_FOUND"
+//        );
+//
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
 }
 
--- src/main/java/mg/java/springbootrestapi/dto/UserDto.java
+++ src/main/java/mg/java/springbootrestapi/dto/UserDto.java
@@ -1,5 +1,6 @@
 package mg.java.springbootrestapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "UserDto Model Information"
+)
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,13 +19,24 @@ public class UserDto {
 
     private Long id;
 
+    @Schema(
+            description = "User First Name"
+    )
+
     // User first name should not be null or empty
     @NotEmpty(message = "User first name should not be null or empty")
     private String firstName;
+
+    @Schema(
+            description = "User Last Name"
+    )
     // User last name should not be null or empty
     @NotEmpty(message = "User last name should not be null or empty")
     private String lastName;
 
+    @Schema(
+            description = "User Email Address"
+    )
     // User email should not be null or empty and email address should be valid
     @NotEmpty(message = "User email should not be null or empty")
     @Email(message = "User Email address should be valid")
